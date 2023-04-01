239aac53fea92495c2a5053078dd4e76a4d87703
Shabari02 <lkshabarinathan02@gmail.com>
2023-04-02 00:25:30 +0530

    all-post  updated

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,3 +1,38 @@
-function AllPostsPage() {}
+import AllPosts from "../../components/posts/all-posts";
+
+const DUMMY_POSTS = [
+  {
+    slug: "getting-started-nextjs",
+    title: "getting-started",
+    image: "getting-started-nextjs.png",
+    excerpt: "Next js page",
+    date: "02-1-2022",
+  },
+  {
+    slug: "getting-started-nextjs2",
+    title: "getting-started",
+    image: "getting-started-nextjs.png",
+    excerpt: "Next js page",
+    date: "02-1-2022",
+  },
+  {
+    slug: "getting-started-nextjs3",
+    title: "getting-started",
+    image: "getting-started-nextjs.png",
+    excerpt: "Next js page",
+    date: "02-1-2022",
+  },
+  {
+    slug: "getting-started-nextjs4",
+    title: "getting-started",
+    image: "getting-started-nextjs.png",
+    excerpt: "Next js page",
+    date: "02-1-2022",
+  },
+];
+
+function AllPostsPage() {
+  return <AllPosts posts={DUMMY_POSTS}/>;
+}
 
 export default AllPostsPage;
