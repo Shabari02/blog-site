d704511620e3c72a6cc531c48cd3749850cf3dae
Shabari02 <lkshabarinathan02@gmail.com>
2023-04-02 21:18:24 +0530

    logo title changed

--- components/layout/logo.js
+++ components/layout/logo.js
@@ -2,7 +2,7 @@ import classes from './logo.module.css'
 
 
 function Logo() {
-    return <div className={classes.logo}>Shabari Next's blog</div>
+    return <div className={classes.logo}>Shabari's blog</div>
 }
 
 export default Logo;
