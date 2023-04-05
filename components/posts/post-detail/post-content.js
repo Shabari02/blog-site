d8ccd2934587ffb0eb400fed5062223ccf9049e7
Shabari02 <lkshabarinathan02@gmail.com>
2023-04-05 20:01:45 +0530

    updated dynamic route

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -4,22 +4,24 @@ import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
 
 
 
-const DUMMY_POSTS = 
-  {
-    slug: "getting-started-nextjs",
-    title: "getting-started",
-    image: "getting-started-nextjs.png",
-    excerpt: "Next js page",
-    date: "02-1-2022",
-    content: '# This is a first post'
-  }
+// const post = 
+//   {
+//     slug: "getting-started-nextjs",
+//     title: "getting-started",
+//     image: "getting-started-nextjs.png",
+//     excerpt: "Next js page",
+//     date: "02-1-2022",
+//     content: '# This is a first post'
+//   }
 
-function PostContent() {
-    const imagePath = `/images/posts/${DUMMY_POSTS.slug}/${DUMMY_POSTS.image}`
+function PostContent(props) {
+  const { post } = props;
+  console.log(post)
+    const imagePath = `/images/posts/${post.slug}/${post.image}`
   return (
     <article className={classes.content}>
-      <PostHeader title={DUMMY_POSTS.title} image={imagePath} />
-      <ReactMarkdown>{DUMMY_POSTS.content}</ReactMarkdown>
+      <PostHeader title={post.title} image={imagePath} />
+      <ReactMarkdown>{post.content}</ReactMarkdown>
     </article>
   );
 }
