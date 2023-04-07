22194b1d36f6f43b844d2687e597d62f9e096ad2
Shabari02 <lkshabarinathan02@gmail.com>
2023-04-08 02:32:59 +0530

    mongoDb connected

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const { email, name, message } = req.body;
     if (
@@ -19,7 +21,25 @@ function handler(req, res) {
       name,
       message,
     };
-    console.log(newMessage);
+    let client;
+    try {
+      client = await MongoClient.connect(
+        "mongodb+srv://lkshabari:lkts02@cluster0.cy2qnsi.mongodb.net/?retryWrites=true&w=majority"
+      );
+    } catch (error) {
+      res.status(500).json({ message: "Could not connect to database. " });
+      return;
+    }
+    const db = client.db();
+    try {
+      const result = await db.collection("messages").insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (error) {
+      client.close();
+      res.status(500).json({ message: "Storing message failed" });
+      return;
+    }
+    client.close();
     res
       .status(201)
       .json({ message: "Successfully stored message!", message: newMessage });
