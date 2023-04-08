3b3ecaa30d14ba8ca4f806f99e402fd236f4b960
Shabari02 <lkshabarinathan02@gmail.com>
2023-04-08 15:21:51 +0530

    env edited

--- next.config.js
+++ next.config.js
@@ -13,9 +13,9 @@ module.exports = (phase) => {
 
   return {
     env: {
-      mongodb_username: 'maximilian',
-      mongodb_password: '2YkcXq43KyPk0vqp',
-      mongodb_clustername: 'cluster0'
+        mongodb_username: 'lkshabari',
+        mongodb_password: 'lkts02',
+        mongodb_clustername: 'cluster0'
     },
   };
 };
