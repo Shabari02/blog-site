351e074ce8c6ce088456329841bd53cfa89eb5b3
Shabari02 <lkshabarinathan02@gmail.com>
2023-04-08 17:23:41 +0530

    Version updated

--- next.config.js
+++ next.config.js
@@ -1,21 +1,14 @@
-const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
-
-module.exports = (phase) => {
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    return {
-      env: {
-        mongodb_username: 'lkshabari',
-        mongodb_password: 'lkts02',
-        mongodb_clustername: 'cluster0'
-      },
-    };
-  }
-
-  return {
-    env: {
-        mongodb_username: 'lkshabari',
-        mongodb_password: 'lkts02',
-        mongodb_clustername: 'cluster0'
-    },
-  };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    newNextLinkBehavior: false,
+  },
+  env: {
+    mongodb_username: "lkshabari",
+    mongodb_password: "lkts02",
+    mongodb_clustername: "cluster0",
+  },
 };
+
+module.exports = nextConfig;
